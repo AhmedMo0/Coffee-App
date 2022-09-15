f3df529aa4a5da19bf95f7b9e081a4bad1981817
Ahmed Mohamed <54334167+AhmedMo0@users.noreply.github.com>
2022-09-15 09:55:46 +0200

    Update CartProductController.java

--- src/main/java/com/example/demo/controllers/CartProductController.java
+++ src/main/java/com/example/demo/controllers/CartProductController.java
@@ -1,8 +1,14 @@
 package com.example.demo.controllers;
 
+import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,7 +16,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.example.demo.domain.CartProduct;
 import com.example.demo.domain.ItemDto;
@@ -24,16 +32,29 @@ public class CartProductController {
 	@Autowired
 	CartProductService cartProductService;
 
-	/*
+	
+	@Cacheable(value = "cartProducts")
 	@GetMapping(value = { "", "/" })
 	public List<CartProduct> getAllProducts() {
-		return cartProductService.getAllProducts();
+		try {
+			
+			return cartProductService.getAllProducts();
+			
+		} catch (Exception e) {
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+		}
 	}
-	*/
 
+	@Cacheable(value = "cartProducts", key = "#userId")
 	@GetMapping(value = "/{userId}")
 	public List<CartProduct> getCart(@PathVariable long userId) {
-		return cartProductService.getCart(userId);
+		try {			
+			
+			return cartProductService.getCart(userId);
+			
+		} catch (Exception e) {
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+		}
 	}
 
 	@GetMapping(value = "/cardtotal/{cardId}")
@@ -41,24 +62,49 @@ public class CartProductController {
 		return cartProductService.getCardTotal(cardId);
 	}
 
+	@CacheEvict(value= "cartProducts", allEntries = true)
 	@PostMapping(value = {"", "/addProductToCart"})
-	public void addItemToCart(@RequestBody ItemDto item) {
-		cartProductService.addItemToCart(item);
+	public ResponseEntity<?> addItemToCart(@RequestBody ItemDto item) {
+		try{
+			cartProductService.addItemToCart(item);
+			
+			return ResponseEntity.ok().body(null);
+		} catch (Exception e) {
+			return ResponseEntity.badRequest().body(null);
+		}
 	}
 	
+	@CacheEvict(value= "cartProducts", allEntries = true)
 	@PostMapping(value = {"/addAllProducts"})
-	public void addAllItemToCart(@RequestBody List<ItemDto> items) {
-		cartProductService.addAllItemToCart(items);
+	public ResponseEntity<?> addAllItemToCart(@RequestBody List<ItemDto> items) {
+		try {
+			cartProductService.addAllItemToCart(items);
+			return ResponseEntity.ok().body(null);
+		} catch (Exception e) {
+			return ResponseEntity.badRequest().body(null);
+		}
 	}
 	
+	@CacheEvict(value= "cartProducts", allEntries = true)
 	@DeleteMapping(value = {"/deleteproduct/{cartProductId}", "/buyproduct/{cartProductId}"})
-	public void deleteProductFromCart(@PathVariable int cartProductId) {
-		cartProductService.deleteProductFromCart(cartProductId);
+	public ResponseEntity<?> deleteProductFromCart(@PathVariable int cartProductId) {
+		try {
+			cartProductService.deleteProductFromCart(cartProductId);
+			return ResponseEntity.ok().body(null);
+		} catch (Exception e) {
+			return ResponseEntity.badRequest().body(null);
+		}
 	}
 	
+	@CacheEvict(value= "cartProducts", allEntries = true)
 	@DeleteMapping(value = {"/deleteCart/{userId}", "/buy/{userId}"})
-	public void deleteCart(@PathVariable Long userId) {
-		cartProductService.deleteCart(userId);
+	public ResponseEntity<?> deleteCart(@PathVariable Long userId) {
+		try {
+			cartProductService.deleteCart(userId);	
+			return ResponseEntity.ok().body(null);
+		} catch (Exception e) {
+			return ResponseEntity.badRequest().body(null);
+		}
 	}
 	
 
