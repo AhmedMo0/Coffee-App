badfda6ef3fc1fd8d5ef989ef35ef6f28f01c4cf
Ahmed Mohamed <54334167+AhmedMo0@users.noreply.github.com>
2022-09-15 09:58:56 +0200

    Update ProductController.java

--- src/main/java/com/example/demo/controllers/ProductController.java
+++ src/main/java/com/example/demo/controllers/ProductController.java
@@ -1,6 +1,9 @@
 package com.example.demo.controllers;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +13,8 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
 import com.example.demo.domain.Product;
 import com.example.demo.Service.ProductService;
 
@@ -20,53 +25,99 @@ public class ProductController {
 	@Autowired
 	ProductService productService;
 
+	
+	@Cacheable(value = "products")
 	@GetMapping(value = { "", "/" })
 	public List<Product> getAllProducts() {
-		return productService.getAllProducts();
+		try {
+			return productService.getAllProducts();
+		} catch (Exception e) {
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+		}
 	}
 
 	@GetMapping(value = "/{id}")
 	public Product getProductWithID(@PathVariable int id) {
-		return productService.getProductWithID(id);
+		try {
+			return productService.getProductWithID(id);
+		} catch (Exception e) {
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+		}
 	}
 
 	@GetMapping(value = "/cat/{category}")
 	public List<Product> getProductWithCategory(@PathVariable String category) {
-		return productService.getProductWithCategory(category);
+		try {
+			return productService.getProductWithCategory(category);
+		} catch (Exception e) {
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+		}
 	}
 
 	@GetMapping(value = "/card/{cardId}")
 	public List<Product> getProductWithCardId(@PathVariable int cardId) {
-		return productService.getProductWithCardId(cardId);
+		try {
+			return productService.getProductWithCardId(cardId);
+		} catch (Exception e) {
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+		}
 	}
 
+	@CacheEvict(value= "products", allEntries = true)
 	@PostMapping(value = "/add")
 	public Product addProduct(@RequestBody Product product) {
-		return productService.addAll(product);
+		try {
+			return productService.addAll(product);
+		} catch (Exception e) {
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+		}
 	}
-
+	
+	@CacheEvict(value= "products", allEntries = true)
 	@PostMapping(value = "/addAll")
 	public List<Product> addProduct(@RequestBody List<Product> products) {
-		return productService.addAll(products);
+		try {
+			return productService.addAll(products);
+		} catch (Exception e) {
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+		}
 	}
-
+	
+	@CacheEvict(value= "products", allEntries = true, key = "#id")
 	@DeleteMapping(value = "/del/{id}")
 	public String delProduct(@PathVariable int id) {
-		return productService.delProduct(id);
+		try {
+			return productService.delProduct(id);
+		} catch (Exception e) {
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+		}
 	}
 
+	
 	@PutMapping(value = "/updateProduct")
 	public Product updateProduct(@RequestBody Product nProduct) {
-		return productService.updateProduct(nProduct);
+		try {
+			return productService.updateProduct(nProduct);
+		} catch (Exception e) {
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+		}
 	}
 
 	@PutMapping(value = "/updateProductName")
 	public Product updateProductName(@RequestBody Product nProduct) {
-		return productService.updateProductName(nProduct);
+		try{
+			return productService.updateProductName(nProduct);
+		} catch (Exception e) {
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+		}
 	}
 
 	@PutMapping(value = "/updateProductAddToCart")
 	public Product updateProductAddToCart(@RequestBody Product nProduct) {
-		return productService.updateProductAddToCart(nProduct);
+		try{
+			return productService.updateProductAddToCart(nProduct);
+		} catch (Exception e) {
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+		}
 	}
 }
